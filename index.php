8f357e4fc630e3624a0759bb206201c5d1825b51
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-01-18 20:47:50 +0100

    dynamically get the posts

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 
+get_header();
 if (have_posts()):
-    while (have_posts()): the_post();
-        the_title();
-        the_content();
-    endwhile;
+    while (have_posts()): the_post(); ?>
+        <h2>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+            </a>
+        </h2>
+        <p><?php the_content(); ?></p>
+
+    <?php endwhile;
+else:
+    ?>
+    <p>Sorry, no posts found.</p>
+<?php
 endif;
+
+
+get_footer();
