08e39f9b80dc1d327f935a857621c06b180952a4
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-01-19 08:41:02 +0100

    add footer and header

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <footer class="site-footer">
+        <p>
+            <span>
+                <?php bloginfo('name'); ?>
+            </span>
+            <span>&copy;</span>
+            <span><?php echo date('Y'); ?></span>
+        </p>
+    </footer>
+
+</div> <!-- /container -->
+<?php wp_footer(); ?>
+</body>
+
+</html>
