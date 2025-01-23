128c66ec4cf9f7495bb59ca0203e1872169dc21e
Petya Naydenova Ferreira <pn@abtion.com>
2025-01-23 13:56:50 +0100

    add search form to header

--- header.php
+++ header.php
@@ -11,18 +11,21 @@
 <body <?php body_class(); ?>>
     <div class="container">
         <header class="site-header">
-            <h1>
-                <a href="<?php echo home_url(); ?>">
-                    <?php bloginfo('name'); ?>
-                </a>
-            </h1>
+            <div class="title-search">
+                <h1>
+                    <a href="<?php echo home_url(); ?>">
+                        <?php bloginfo('name'); ?>
+                    </a>
+                </h1>
+                <div class="header-search">
+                    <?php get_search_form(); ?>
+                </div>
+            </div>
             <p><?php bloginfo('description'); ?>
                 <?php if (is_page('my-projects')) { ?>
                     - thank you for viewing my projects
                 <?php } ?>
             </p>
-
-
 
             <nav class="site-nav">
                 <?php
@@ -30,7 +33,6 @@
                     "theme_location" => "primary"
                 )
                 ?>
-
                 <?php wp_nav_menu($args); ?>
             </nav>
         </header>
