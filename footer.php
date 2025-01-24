79b4cd2dafe0681d4bfaf50937649a09f0bcc282
Petya Naydenova Ferreira <pn@abtion.com>
2025-01-24 14:51:42 +0100

    add widgets areas to footer

--- footer.php
+++ footer.php
@@ -1,11 +1,33 @@
     <footer class="site-footer">
-        <?php 
+        <?php
         $args = array(
             "theme_location" => "footer"
         )
         ?>
-
+        <div class="footer-widgets">
+            <?php if (is_active_sidebar('footer1')): ?>
+                <div>
+                    <?php dynamic_sidebar('footer1') ?>
+                </div>
+            <?php endif; ?>
+            <?php if (is_active_sidebar('footer2')): ?>
+                <div>
+                    <?php dynamic_sidebar('footer2') ?>
+                </div>
+            <?php endif; ?>
+            <?php if (is_active_sidebar('footer3')): ?>
+                <div>
+                    <?php dynamic_sidebar('footer3') ?>
+                </div>
+            <?php endif; ?>
+            <?php if (is_active_sidebar('footer4')): ?>
+                <div>
+                    <?php dynamic_sidebar('footer4') ?>
+                </div>
+            <?php endif; ?>
+        </div>
         <nav class="site-nav"> <?php wp_nav_menu($args); ?></nav>
+
         <p>
             <span>
                 <?php bloginfo('name'); ?>
@@ -13,6 +35,7 @@
             <span>&copy;</span>
             <span><?php echo date('Y'); ?></span>
         </p>
+
     </footer>
 
     </div> <!-- /container -->
