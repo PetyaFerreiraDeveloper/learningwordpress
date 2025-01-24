94db634c529e987c5d567c6150c6aec089cb973e
Petya Naydenova Ferreira <pn@abtion.com>
2025-01-24 09:34:33 +0100

    create content.php template to use it on different  pages

--- archive.php
+++ archive.php
@@ -20,9 +20,9 @@ if (have_posts()):
     } else if (is_day()) {
       echo 'This is a daily archive ' . get_the_date();
     } else if (is_month()) {
-      echo 'This is a monthly archive ' .get_the_date('F Y');
+      echo 'This is a monthly archive ' . get_the_date('F Y');
     } else if (is_year()) {
-      echo 'This is a yearly archive ' .get_the_date('Y');
+      echo 'This is a yearly archive ' . get_the_date('Y');
     } else {
       echo 'This is an archive';
     }
@@ -30,33 +30,10 @@ if (have_posts()):
 
   </h2>
 
-  <?php
-  while (have_posts()): the_post(); ?>
-    <article class="post">
-      <h3>
-        <a href="<?php the_permalink(); ?>">
-          <?php the_title() ?>
-        </a>
-      </h3>
-      <p class="post-info">
-        <?php the_time('j F, Y') ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>"> <?php the_author(); ?> </a> | Posted in
-        <?php
-        $categories = get_the_category();
-        $separator = ', ';
-        $output = '';
-
-        if ($categories) {
-          foreach ($categories as $category) {
-            $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
-            echo trim($output, $separator);
-          }
-        }
-        ?>
-      </p>
-      <p><?php the_excerpt() ?></p>
-    </article>
-
-<?php endwhile;
+<?php
+  while (have_posts()): the_post();
+    get_template_part('content');
+  endwhile;
 else:
   echo '<p>No content found</p>';
 endif;
